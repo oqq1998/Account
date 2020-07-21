cadd16f0fa225e02b3970c21251d2902b9c1930d
oqqkun <oqqkun@foxmail.com>
2020-07-21 23:59:36 +0800

    svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="nav">      
-        <router-link to="/account">记账</router-link> | 
-        <router-link to="/labels">标签</router-link> | 
-        <router-link to="/statistics">统计</router-link>
+    <div class="nav">     
+        <router-link to="/labels">
+            <!-- <svg>
+                <use xlink:href="#Labels"/>
+            </svg> -->
+            标签
+        </router-link> | 
+        <router-link to="/account">
+            记账
+        </router-link> | 
+        <router-link to="/statistics">
+            统计
+        </router-link>
     </div>
 </template>
 
 <script lang="ts">
+    // import x from '@/assets/icons/Labels.svg'
     export default {
         name:"Nav"
     }
